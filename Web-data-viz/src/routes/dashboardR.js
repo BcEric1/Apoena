d2e15effbb056815c1cbb3f607b77d024b6d3de4
BcEric1 <eric.costa@sptech.school>
2025-11-23 19:29:11 -0300

    Route de Dashbaord

---
+++ Web-data-viz/src/routes/dashboardR.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+var dashboardController = require("../controllers/dashboardController");
+
+// Rota para cadastrar perfil
+router.post("/cadastrar", function (req, res) {
+    dashboardController.cadastrarPerfil(req, res);
+});
+
+// Rota para buscar último perfil do usuário
+router.get("/ultimo/:idUsuario", function (req, res) {
+    dashboardController.buscarUltimoPerfil(req, res);
+});
+
+// Rota para buscar respostas de um perfil específico
+router.get("/respostas/:idPerfil", function (req, res) {
+    dashboardController.buscarRespostasPerfil(req, res);
+});
+
+module.exports = router;
